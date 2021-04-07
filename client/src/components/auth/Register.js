1b80f4f589eae98b2be002d71dad1cf1c218b150
Ashish <ashishDongol32@gmail.com>
2021-04-07 08:43:16 +0545

    register form implemented

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Regiser = () => {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+    errors: {}
+  });
+
+  const { name, email, password, password2, errors } = user;
+
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newUser = {
+      name,
+      email,
+      password,
+      password2
+    };
+
+    console.log(newUser);
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
+    <div className='register'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-8 m-auto'>
+            <h1 className='display-4 text-center'>Sign Up</h1>
+            <p className='lead text-center'>Create your DevConnector account</p>
+            <form onSubmit={onSubmit}>
+              <div className='form-group'>
+                <input
+                  type='text'
+                  className='form-control form-control-lg'
+                  placeholder='Name'
+                  name='name'
+                  value={name}
+                  onChange={onChange}
+                  required
+                />
+              </div>
+              <div className='form-group'>
+                <input
+                  type='email'
+                  className='form-control form-control-lg'
+                  placeholder='Email Address'
+                  name='email'
+                  value={email}
+                  onChange={onChange}
+                />
+                <small className='form-text text-muted'>
+                  This site uses Gravatar so if you want a profile image, use a
+                  Gravatar email
+                </small>
+              </div>
+              <div className='form-group'>
+                <input
+                  type='password'
+                  className='form-control form-control-lg'
+                  placeholder='Password'
+                  name='password'
+                  value={password}
+                  onChange={onChange}
+                />
+              </div>
+              <div className='form-group'>
+                <input
+                  type='password'
+                  className='form-control form-control-lg'
+                  placeholder='Confirm Password'
+                  name='password2'
+                  value={password2}
+                  onChange={onChange}
+                />
+              </div>
+              <input type='submit' className='btn btn-info btn-block mt-4' />
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
